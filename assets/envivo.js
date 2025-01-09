86b007968ea80697cacc666191418a86fbf2690c
redy <granredy@gmail.com>
2025-01-09 14:07:33 -0500

    Actualizar el archivo assets/envivo.js

--- assets/envivo.js
+++ assets/envivo.js
@@ -1,7 +1,258 @@
 const horario_envivo = {
+  "html": {
+    "value": "<iframe id=\"vrudo\" class=\"vrudo\" src=\"//rudo.video/live/ecuavisaradio\" width=\"410\" height=\"550\" allowfullscreen=\"true\" frameborder=\"0\" scrolling=\"no\" allow=\"autoplay; fullscreen\"></iframe>"
+  },
   "forzado": {
     "estado": false,
     "titulo": "En Vivo",
     "label": "AUDIO"
-  }
+  },
+  "horarios": [
+    {
+      "dia": 0,
+      "estadoDia": true,
+      "horas": [
+        {
+          "tituloPrograma": "Políticamente Correcto",
+          "estadoHorario": true,
+          "inicio": "10:30",
+          "fin": "11:30"
+        },
+        {
+          "tituloPrograma": "Televistazo 19h00",
+          "estadoHorario": true,
+          "inicio": "19:00",
+          "fin": "20:00"
+        }
+      ]
+    },
+    {
+      "dia": 1,
+      "estadoDia": true,
+      "horas": [
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "05:55",
+          "fin": "07:00"
+        },
+        {
+          "tituloPrograma": "Contacto Directo",
+          "estadoHorario": true,
+          "inicio": "07:00",
+          "fin": "07:30"
+        },
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "07:30",
+          "fin": "09:00"
+        },
+        {
+          "tituloPrograma": "Televistazo 13h00",
+          "estadoHorario": true,
+          "inicio": "13:00",
+          "fin": "14:00"
+        },
+        {
+          "tituloPrograma": "Hackers de la Farandula",
+          "estadoHorario": false,
+          "inicio": "14:00",
+          "fin": "15:30"
+        },
+        {
+          "tituloPrograma": "Televistazo 19h00",
+          "estadoHorario": true,
+          "inicio": "19:00",
+          "fin": "20:30"
+        }
+      ]
+    },
+    {
+      "dia": 2,
+      "estadoDia": true,
+      "horas": [
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "05:55",
+          "fin": "07:00"
+        },
+        {
+          "tituloPrograma": "Contacto Directo",
+          "estadoHorario": true,
+          "inicio": "07:00",
+          "fin": "07:30"
+        },
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "07:30",
+          "fin": "09:00"
+        },
+        {
+          "tituloPrograma": "En Contacto",
+          "estadoHorario": false,
+          "inicio": "10:30",
+          "fin": "13:00"
+        },
+        {
+          "tituloPrograma": "Televistazo 13h00",
+          "estadoHorario": true,
+          "inicio": "13:00",
+          "fin": "14:00"
+        },
+        {
+          "tituloPrograma": "Hackers de la Farandula",
+          "estadoHorario": false,
+          "inicio": "14:00",
+          "fin": "15:30"
+        },
+        {
+          "tituloPrograma": "Televistazo 19h00",
+          "estadoHorario": true,
+          "inicio": "19:00",
+          "fin": "20:30"
+        }
+      ]
+    },
+    {
+      "dia": 3,
+      "estadoDia": true,
+      "horas": [
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "05:55",
+          "fin": "07:00"
+        },
+        {
+          "tituloPrograma": "Contacto Directo",
+          "estadoHorario": true,
+          "inicio": "07:00",
+          "fin": "07:30"
+        },
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "07:03",
+          "fin": "09:00"
+        },
+        {
+          "tituloPrograma": "Televistazo 13h00",
+          "estadoHorario": true,
+          "inicio": "13:00",
+          "fin": "14:00"
+        },
+        {
+          "tituloPrograma": "Hackers de la Farandula",
+          "estadoHorario": false,
+          "inicio": "14:00",
+          "fin": "15:30"
+        },
+        {
+          "tituloPrograma": "Televistazo 19h00",
+          "estadoHorario": true,
+          "inicio": "19:00",
+          "fin": "20:30"
+        }
+      ]
+    },
+    {
+      "dia": 4,
+      "estadoDia": true,
+      "horas": [
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "05:55",
+          "fin": "07:00"
+        },
+        {
+          "tituloPrograma": "Contacto Directo",
+          "estadoHorario": true,
+          "inicio": "07:00",
+          "fin": "07:30"
+        },
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "07:30",
+          "fin": "09:00"
+        },
+        {
+          "tituloPrograma": "Televistazo 13h00",
+          "estadoHorario": true,
+          "inicio": "13:00",
+          "fin": "14:00"
+        },
+        {
+          "tituloPrograma": "Hackers de la Farandula",
+          "estadoHorario": false,
+          "inicio": "14:00",
+          "fin": "15:30"
+        },
+        {
+          "tituloPrograma": "Televistazo 19h00",
+          "estadoHorario": true,
+          "inicio": "19:00",
+          "fin": "20:30"
+        }
+      ]
+    },
+    {
+      "dia": 5,
+      "estadoDia": true,
+      "horas": [
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "05:55",
+          "fin": "07:00"
+        },
+        {
+          "tituloPrograma": "Contacto Directo",
+          "estadoHorario": true,
+          "inicio": "07:00",
+          "fin": "07:30"
+        },
+        {
+          "tituloPrograma": "Televistazo en la comunidad",
+          "estadoHorario": true,
+          "inicio": "07:30",
+          "fin": "09:00"
+        },
+        {
+          "tituloPrograma": "Televistazo 13h00",
+          "estadoHorario": true,
+          "inicio": "13:00",
+          "fin": "14:00"
+        },
+        {
+          "tituloPrograma": "Hackers de la Farandula",
+          "estadoHorario": false,
+          "inicio": "14:00",
+          "fin": "15:30"
+        },
+        {
+          "tituloPrograma": "Televistazo 19h00",
+          "estadoHorario": true,
+          "inicio": "19:00",
+          "fin": "20:30"
+        }
+      ]
+    },
+    {
+      "dia": 6,
+      "estadoDia": true,
+      "horas": [
+        {
+          "tituloPrograma": "Televistazo 19h00",
+          "estadoHorario": true,
+          "inicio": "19:00",
+          "fin": "19:30"
+        }
+      ]
+    }
+  ]
 };
