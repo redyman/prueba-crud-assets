1abae3396a0e8b53dd67f5e1205451d61d4e4423
granredy <granredy@gmail.com>
2025-01-21 13:02:30 -0500

    add nuevos archivos de prueba

---
+++ api/index.js
@@ -0,0 +1,36 @@
+require("../config/database.js");
+const { PORT } = require("../config/variable.js");
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const axios = require('axios');
+
+// index.js
+const route_live_web = require('./../src/live-web.js');
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+// version del app
+app.get('/', (req, res) => {
+    res.json({ name: 'servicio-github-dynamic-live', version: '1.0.0' });
+});
+
+/***************************************************/
+// competencia: El Comercio con playwright scraper //
+/***************************************************/
+
+// Aplicar las rutas live-web
+app.use('/live-web', route_live_web);
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+module.exports = app;
