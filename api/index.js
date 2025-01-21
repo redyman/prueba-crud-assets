9e4abafa96b72a74da64db450251da9a42033fd6
granredy <granredy@gmail.com>
2025-01-21 17:45:30 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // version del app
 app.get('/', (req, res) => {
-    res.json({ name: 'servicio-github-dynamic-live', version: '1.0.6' });
+    res.json({ name: 'servicio-github-dynamic-live', version: '1.0.7' });
 });
 
 /***************************************************/
