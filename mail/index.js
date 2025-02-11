02af345f545c0e3c9363faffbcec311ce555c3c5
Carlos Rojano <granredy@gmail.com>
2025-02-11 18:02:07 -0500

    Update index.js

--- mail/index.js
+++ mail/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 app.get('/', async function(req, res) {
   
-  res.json("mail API 1.9")   
+  res.json("mail API 2")   
 })
 app.post('/', function(req, res) {
    
